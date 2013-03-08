a8e5be1ff33d3c5d01f683242b20657f951f41f8
Maxime <Maxime@10.7.205.216>
2013-03-08 16:46:19 +0100

    uio

--- roboSym/src/SimulatorLauncher.java
+++ roboSym/src/SimulatorLauncher.java
@@ -3,13 +3,10 @@ import java.awt.event.*;
 
 public class SimulatorLauncher implements ActionListener {
 	
-    public void actionPerformed(ActionEvent arg0) {
-      SimulatorInterface si = new SimulatorInterface();
-      arg0.getSource();
-    }
     
-    SimulatorLauncher(){
+    
+ /*   SimulatorLauncher(){
     	super();
-    }
+    }*/
     
  }

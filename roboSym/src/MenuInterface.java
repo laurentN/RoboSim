9ca68a34642393f0df8e1ebbf591989028eb4c57
Loic <Loic@10.7.253.124>
2013-03-08 17:26:30 +0100

    Modif

--- roboSym/src/MenuInterface.java
+++ roboSym/src/MenuInterface.java
@@ -1,31 +1,20 @@
 import java.awt.EventQueue;
-
-import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
 import java.awt.Font;
-
-import javax.swing.JOptionPane;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.image.BufferedImage;
-
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
-import java.awt.Canvas;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Panel;
-import javax.swing.ImageIcon;
-import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.swing.text.html.ImageView;
-import javax.swing.text.Element;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
 
 import ui.UICreateRobot;
 

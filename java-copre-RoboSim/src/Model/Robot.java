b1c30ab8542e0486cbf9aa5ccc58e2d4c3a93411
Loic <Loic@10.7.253.122>
2013-03-08 09:58:26 +0100

    JDom Added

--- java-copre-RoboSim/src/Model/Robot.java
+++ java-copre-RoboSim/src/Model/Robot.java
@@ -18,12 +18,10 @@ public class Robot extends ARobot{
 	}
 	public Robot(int speed, String name, int xPosition, int yPosistion) {
 		super(speed, name, xPosition, yPosistion);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String getDescription() {
-		// TODO Auto-generated method stub
 		return "Sensor List :";
 	}
 
